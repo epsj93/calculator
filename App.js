58aaa6c95d1b1497d4fc38f42fefa621397c506f
epsj93 <epsj.93@gmail.com>
2022-08-12 20:22:34 -0300

    button update

--- App.js
+++ App.js
@@ -6,31 +6,31 @@ import Display from './src/components/Display';
 const App = () => {
   const [displayValue, setDisplayValue] = useState('0');
 
-  function addDigit(n) {
-    setDisplayValue({ displayValue: n });
-  }
+  const addDigit = (n) => setDisplayValue(n);
+  const clearMemory = () => setDisplayValue('0');
+  const setOperation = () => {};
 
   return (
     <View style={styles.container}>
       <Display value={displayValue} />
       <View style={styles.buttons}>
-        <Button label="AC" triple />
-        <Button label="/" operation />
+        <Button label="AC" triple onClick={clearMemory} />
+        <Button label="/" operation onClick={setOperation} />
         <Button label="7" onClick={addDigit} />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="*" operation />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" operation />
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" operation />
-        <Button label="0" double />
-        <Button label="." />
-        <Button label="=" operation />
+        <Button label="8" onClick={addDigit} />
+        <Button label="9" onClick={addDigit} />
+        <Button label="*" operation onClick={setOperation} />
+        <Button label="4" onClick={addDigit} />
+        <Button label="5" onClick={addDigit} />
+        <Button label="6" onClick={addDigit} />
+        <Button label="-" operation onClick={setOperation} />
+        <Button label="1" onClick={addDigit} />
+        <Button label="2" onClick={addDigit} />
+        <Button label="3" onClick={addDigit} />
+        <Button label="+" operation onClick={setOperation} />
+        <Button label="0" double onClick={addDigit} />
+        <Button label="." onClick={addDigit} />
+        <Button label="=" operation onClick={setOperation} />
       </View>
     </View>
   );
